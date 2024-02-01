d0bfdde57c5423fd60d3760d8fe98a6c07f10dd1
Bemoi Erian <bemoi.erian@gmail.com>
2024-02-01 20:47:47 +0200

    added assets folder for each module

---
+++ Assets/GlobalAssets/Scripts/UI/SceneChanger.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace GlobalAssets.UI
+{
+    public class SceneChanger : MonoBehaviour
+    {
+        public void LoadScene(string targetSceneName)
+        {
+            SceneManager.LoadScene(targetSceneName);
+        }
+    }
+
+}
