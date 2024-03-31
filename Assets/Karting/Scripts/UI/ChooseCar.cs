a1cf3c103198813cfd35641d29c0c47f366333dd
Bemoi Erian <bemoi.erian@gmail.com>
2024-03-31 17:57:45 +0200

    swipe between cars in choose car scene

--- Assets/Karting/Scripts/UI/ChooseCar.cs
+++ Assets/Karting/Scripts/UI/ChooseCar.cs
@@ -1,17 +1,69 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 namespace Karting.UI
 {
     public class ChooseCar : MonoBehaviour
     {
-        public Button DodgeButton;
-        public GameObject DodgeCarPrefab;
+        public Button selectButton;
+        public List<GameObject> carPrefabs;
+        private int currentCarIndex = 0;
+        public Transform carSpawnPoint;
+        private GameObject instantiatedCar;
+
         void Start()
         {
+            // Get the GameManager instance
             Game.GameManager gameManager = FindObjectOfType<Game.GameManager>();
-            DodgeButton.onClick.AddListener(() => gameManager.SelectCar(DodgeCarPrefab));
-            DodgeButton.onClick.AddListener(() => gameManager.StartCustomMatch());
+            // Add a listener to the select button
+            // When the button is clicked, select the current car of the game manager
+            selectButton.onClick.AddListener(() => SelectCurrentCar(gameManager));
+            // if this is the scene before the match, start the match
+            selectButton.onClick.AddListener(() => gameManager.StartCustomMatch());
+            InstantiateCurrentCar();
+        }
+
+        private void SelectCurrentCar(Game.GameManager gameManager)
+        {
+            if (currentCarIndex < carPrefabs.Count)
+            {
+                GameObject selectedCarPrefab = carPrefabs[currentCarIndex];
+                gameManager.SelectCar(selectedCarPrefab);
+            }
+            else
+            {
+                Debug.LogError("Invalid car index!");
+            }
+        }
+        private void DestroyInstantiatedCar()
+        {
+            instantiatedCar.SetActive(false);
+            Destroy(instantiatedCar);
+        }
+        private void InstantiateCurrentCar()
+        {
+            instantiatedCar = Instantiate(carPrefabs[currentCarIndex], carSpawnPoint.position, carSpawnPoint.rotation);
+        }
+        public void NextCar()
+        {
+            DestroyInstantiatedCar();
+            currentCarIndex++;
+            if (currentCarIndex >= carPrefabs.Count)
+            {
+                currentCarIndex = 0;
+            }
+            InstantiateCurrentCar();
+        }
+        public void PreviousCar()
+        {
+            DestroyInstantiatedCar();
+            currentCarIndex--;
+            if (currentCarIndex < 0)
+            {
+                currentCarIndex = carPrefabs.Count - 1;
+            }
+            InstantiateCurrentCar();
         }
     }
 }
