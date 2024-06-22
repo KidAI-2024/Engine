0aaa553643e654e2f71099ab5d14421b632a00c1
Bemoi Erian <bemoi.erian@gmail.com>
2024-06-22 16:50:35 +0300

    init checkpoint circle

---
+++ Assets/Karting/Scripts/Game/Checkpoint.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Karting.Game
+{
+    public class Checkpoint : MonoBehaviour
+    {
+        // OnTriggerEnter is called when the Collider other enters the trigger
+        void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("Collided with " + other.gameObject.name);
+            if (other.gameObject.CompareTag("Player"))
+            {
+                Debug.Log("Checkpoint reached");
+            }
+        }
+    }
+}
