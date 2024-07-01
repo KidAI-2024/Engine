d031b640b05ec4ee5cd217e53f568734a056e5ee
Bemoi Erian <bemoi.erian@gmail.com>
2024-07-01 21:02:01 +0300

    init hand pose configuration

---
+++ Assets/GlobalAssets/Scripts/HandPoseTraining/HandPoseConfigureModel.cs
@@ -0,0 +1,190 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace GlobalAssets.HandPoseTraining
+{
+    public class HandPoseConfigureModel : MonoBehaviour
+    {
+        public GameObject features;
+        public GameObject models;
+        public GameObject featureExtarction;
+
+        public GameObject mediaPipeToggle;
+        //public GameObject classicalToggle;
+        public GameObject featureExtractionTypeNotes;
+
+        public GameObject SVMtoggle;
+        public GameObject NeuralNetworktoggle;
+
+        private string mediapipeNote = "High performance, Accurate feature extraction, and Real-time processing.";
+        //private string classicalNote = "Traditional training mode, Time-consuming process.";
+        private ProjectController ProjectController;
+        private GameObject mediapipeSkeleton;
+        //private GameObject classicalWarning;
+
+        void Start()
+        {
+            if (features == null)
+            {
+                Debug.LogError("Features GameObject is not set in the HandPoseConfigureModel script.");
+            }
+            if (mediaPipeToggle == null)
+            {
+                Debug.LogError("MediaPipeToggle GameObject is not set in the HandPoseConfigureModel script.");
+            }
+            
+            if (SVMtoggle != null)
+            {
+
+                SVMtoggle.GetComponent<Toggle>().onValueChanged.AddListener((value) => NeuralNetworktoggle.GetComponent<Toggle>().isOn = !value);
+            }
+            if (NeuralNetworktoggle != null)
+            {
+                NeuralNetworktoggle.GetComponent<Toggle>().onValueChanged.AddListener((value) => SVMtoggle.GetComponent<Toggle>().isOn = !value);
+            }
+            if (mediaPipeToggle != null)
+            {
+                mediaPipeToggle.GetComponent<Toggle>().onValueChanged.AddListener((value) => MediaPipeToggle(value));
+            }
+            //classicalToggle.GetComponent<Toggle>().onValueChanged.AddListener((value) => ClassicalToggle(value));
+
+            //classicalWarning = features.transform.parent.parent.GetChild(2).gameObject;
+
+            ProjectController = ProjectController.Instance;
+            LoadFeatures();
+            LoadModel();
+            LoadTrainingMode();
+        }
+        public void Save()
+        {
+            if (ProjectController == null)
+            {
+                return;
+            }
+            ProjectController.features = GetFeaturesList();
+            ProjectController.model = GetModel();
+            ProjectController.featureExtractionType = GetTrainingMode();
+            ProjectController.Save();
+        }
+
+
+
+        List<string> GetFeaturesList()
+        {
+            List<string> featuresList = new List<string>();
+            // loop on features children (each child is a toggle) if on then add the child.name to the list
+            foreach (Transform child in features.transform)
+            {
+                if (child.GetComponent<Toggle>().isOn)
+                {
+                    featuresList.Add(child.name);
+                }
+            }
+            Debug.Log("Features List: " + string.Join(", ", featuresList));
+            return featuresList;
+        }
+        public void SetModel(string name)
+        {
+            // loop on features children (each child is a toggle) and set all to off
+            foreach (Transform child in models.transform)
+            {
+                if (child.name == name)
+                {
+                    child.GetComponent<Toggle>().isOn = true;
+                }
+                else
+                {
+                    child.GetComponent<Toggle>().isOn = false;
+                }
+            }
+        }
+        string GetModel()
+        {
+            for (int i = 0; i < models.transform.childCount; i++)
+            {
+                if (models.transform.GetChild(i).GetComponent<Toggle>().isOn)
+                {
+                    Debug.Log("Model: " + models.transform.GetChild(i).name);
+                    return models.transform.GetChild(i).name;
+                }
+            }
+            return "";
+        }
+        string GetTrainingMode()
+        {
+            string model = "Classical";
+            if (mediaPipeToggle.GetComponent<Toggle>().isOn)
+            {
+                model = "mediapipe";
+                mediapipeSkeleton.SetActive(true);
+                //classicalWarning.SetActive(false);
+            }
+            else
+            {
+                model = "Classical";
+                mediapipeSkeleton.SetActive(false);
+                //classicalWarning.SetActive(true);
+            }
+            Debug.Log("Training Type: " + model);
+            return model;
+        }
+        void MediaPipeToggle(bool value)
+        {
+            if (value)
+            {
+                //classicalToggle.GetComponent<Toggle>().isOn = false;
+                featureExtractionTypeNotes.GetComponent<TextMeshProUGUI>().text = mediapipeNote;
+                mediapipeSkeleton.SetActive(true);
+                //classicalWarning.SetActive(false);
+            }
+        }
+        void ClassicalToggle(bool value)
+        {
+            if (value)
+            {
+                mediaPipeToggle.GetComponent<Toggle>().isOn = false;
+                //featureExtractionTypeNotes.GetComponent<TextMeshProUGUI>().text = classicalNote;
+                mediapipeSkeleton.SetActive(false);
+                //classicalWarning.SetActive(true);
+            }
+        }
+        void LoadFeatures()
+        {
+            // loop on features children (each child is a toggle) if the child.name is in the ProjectController.features then set the toggle to on
+            foreach (Transform child in features.transform)
+            {
+                if (ProjectController.features.Contains(child.name))
+                {
+                    child.GetComponent<Toggle>().isOn = true;
+                }
+            }
+        }
+        void LoadModel()
+        {
+            if (ProjectController.model == "SVM")
+            {
+                SVMtoggle.GetComponent<Toggle>().isOn = true;
+            }
+            else
+            {
+                NeuralNetworktoggle.GetComponent<Toggle>().isOn = true;
+            }
+        }
+        void LoadTrainingMode()
+        {
+            if (ProjectController.featureExtractionType == "mediapipe")
+            {
+                mediaPipeToggle.GetComponent<Toggle>().isOn = true;
+                featureExtractionTypeNotes.GetComponent<TextMeshProUGUI>().text = mediapipeNote;
+            }
+            else
+            {
+                //classicalToggle.GetComponent<Toggle>().isOn = true;
+                //featureExtractionTypeNotes.GetComponent<TextMeshProUGUI>().text = classicalNote;
+            }
+        }
+    }
+}
---
+++ Assets/GlobalAssets/Scripts/HandPoseTraining/ToggleColorChanger.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+namespace GlobalAssets.HandPoseTraining
+{
+    public class ToggleColorChanger : MonoBehaviour
+    {
+        Toggle toggle;
+        Image toggleBackground;
+
+        void Start()
+        {
+            // Ensure the toggle and image references are set
+            if (toggle == null) toggle = GetComponent<Toggle>();
+            if (toggleBackground == null) toggleBackground = GetComponent<Image>();
+
+            // Set initial color
+            UpdateColor(toggle.isOn);
+
+            // Add listener for when the toggle state changes
+            toggle.onValueChanged.AddListener(UpdateColor);
+        }
+
+        void UpdateColor(bool isOn)
+        {
+            if (isOn)
+            {
+                toggleBackground.color = Color.white; // Change to green when on
+            }
+            else
+            {
+                toggleBackground.color = Color.red; // Change to red when off
+            }
+        }
+
+        void OnDestroy()
+        {
+            // Remove listener when the script is destroyed to avoid memory leaks
+            toggle.onValueChanged.RemoveListener(UpdateColor);
+        }
+    }
+
+}
