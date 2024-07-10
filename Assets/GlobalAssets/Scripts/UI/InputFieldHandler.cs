f51f1f725c8271372092f3a8c5131dc0ee9aa999
EngPeterAtef <peter_atef@stud.cu.edu.eg>
2024-07-10 16:56:42 +0300

    refactor input field by creating input field for each panel and fix try again and exit features by creating a new singleton obj and finally moving door to correct folder

--- Assets/GlobalAssets/Scripts/UI/InputFieldHandler.cs
+++ Assets/GlobalAssets/Scripts/UI/InputFieldHandler.cs
@@ -6,99 +6,203 @@ namespace GlobalAssets.UI
 {
     public class InputFieldHandler : MonoBehaviour
     {
-        public TMP_InputField inputField; // Reference to the Input Field
+        public TMP_InputField inputField1; // Reference to the Input Field
+        public TMP_InputField inputField2; // Reference to the Input Field
         private ProjectController projectController;
-        public Text errorText; // Reference to the Error Text component
+        public Text errorText1; // Reference to the Error Text component
+        public Text errorText2; // Reference to the Error Text component
 
         void Start()
         {
             projectController = ProjectController.Instance;
-            emptyErrorText();
+            string inputFieldName1 = inputField1.name;
+            string inputFieldName2 = inputField2.name;
+
+            errorText1.text = "";
+            errorText2.text = "";
+            if (projectController.isTrained)
+            {
+                Debug.Log("input field1: " + inputFieldName1);
+                Debug.Log("input field2: " + inputFieldName2);
+                if (inputFieldName2 == "model_selection")
+                {
+                    inputField2.text = projectController.classicalModelType.ToString();
+                }
+                else if (inputFieldName1 == "feature_extraction")
+                {
+                    inputField1.text = projectController.featureExtractionTypeImg.ToString();
+                }
+                else if (inputFieldName1 == "epochs")
+                {
+                    inputField1.text = projectController.epochs.ToString();
+                }
+                else if (inputFieldName2 == "learning_rate")
+                {
+                    inputField2.text = projectController.learningRate.ToString();
+                }
+            }
         }
-        public void emptyErrorText()
+        public void emptyError1()
         {
-            errorText.text = "";
+            errorText1.text = "";
+        }
+        public void emptyError2()
+        {
+            errorText2.text = "";
         }
         // read the input field value and set the value of epochs in the project controller
-        public void ReadInputFieldEpochs()
+        public bool ReadInputFieldEpochsResNet()
         {
-            string userInput = inputField.text; // Get the text from the Input Field
+            string userInput = inputField1.text; // Get the text from the Input Field
             // set the value of epochs in the project controller
             try
             {
                 if (int.Parse(userInput) > 0)
                 {
                     projectController.epochs = int.Parse(userInput);
-                    emptyErrorText();
-
+                    errorText1.text = "";
+                    return true;
                 }
                 else
                 {
-                    errorText.text = "Invalid Number Of Epochs. Please enter a number greater than 0";
+                    errorText1.text = "Invalid Number Of Epochs. Please enter a number greater than 0";
                 }
             }
             catch (System.Exception e)
             {
                 Debug.Log(e);
-                errorText.text = "Invalid Number Of Epochs. Please enter a number greater than 0";
+                errorText1.text = "Invalid Number Of Epochs. Please enter a number greater than 0";
             }
-            Debug.Log("User Input: " + projectController.epochs);
+            return false;
         }
         // read the input field value and set the value of learning rate in the project controller
-        public void ReadInputFieldLearningRate()
+        public bool ReadInputFieldLearningRateResNet()
         {
-            string userInput = inputField.text; // Get the text from the Input Field
+            string userInput = inputField2.text; // Get the text from the Input Field
             // set the value of learning rate in the project controller
             try
             {
                 if (float.Parse(userInput) > 0)
                 {
                     projectController.learningRate = float.Parse(userInput);
-                    emptyErrorText();
-
+                    errorText2.text = "";
+                    return true;
                 }
                 else
                 {
 
-                    errorText.text = "Invalid Learning Rate. Please enter a number that is greater than 0";
+                    errorText2.text = "Invalid Learning Rate. Please enter a number that is greater than 0";
                 }
             }
             catch (System.Exception e)
             {
                 Debug.Log(e);
 
-                errorText.text = "Invalid Learning Rate. Please enter a number that is greater than 0";
+                errorText2.text = "Invalid Learning Rate. Please enter a number that is greater than 0";
             }
             Debug.Log("User Input: " + projectController.learningRate);
+            return false;
+        }
+        public void ReadInputResNet()
+        {
+            bool epochsBool = ReadInputFieldEpochsResNet();
+            bool lrBool = ReadInputFieldLearningRateResNet();
+            if (epochsBool && lrBool)
+            {
+                projectController.modelCategory = 1; //Resnet
+            }
+        }
+        // read the input field value and set the value of epochs in the project controller
+        public bool ReadInputFieldEpochsCNN()
+        {
+            string userInput = inputField1.text; // Get the text from the Input Field
+            // set the value of epochs in the project controller
+            try
+            {
+                if (int.Parse(userInput) > 0)
+                {
+                    projectController.epochs = int.Parse(userInput);
+                    errorText1.text = "";
+                    return true;
+                }
+                else
+                {
+                    errorText1.text = "Invalid Number Of Epochs. Please enter a number greater than 0";
+                }
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log(e);
+                errorText1.text = "Invalid Number Of Epochs. Please enter a number greater than 0";
+            }
+            return false;
+        }
+        // read the input field value and set the value of learning rate in the project controller
+        public bool ReadInputFieldLearningRateCNN()
+        {
+            string userInput = inputField2.text; // Get the text from the Input Field
+            // set the value of learning rate in the project controller
+            try
+            {
+                if (float.Parse(userInput) > 0)
+                {
+                    projectController.learningRate = float.Parse(userInput);
+                    errorText2.text = "";
+                    return true;
+                }
+                else
+                {
+
+                    errorText2.text = "Invalid Learning Rate. Please enter a number that is greater than 0";
+                }
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log(e);
+
+                errorText2.text = "Invalid Learning Rate. Please enter a number that is greater than 0";
+            }
+            Debug.Log("User Input: " + projectController.learningRate);
+            return false;
+        }
+        public void ReadInputCNN()
+        {
+            bool epochsBool = ReadInputFieldEpochsCNN();
+            bool lrBool = ReadInputFieldLearningRateCNN();
+            if (epochsBool && lrBool)
+            {
+                projectController.modelCategory = 2; //CNN
+            }
         }
         // read the input field value and set the value of classical model type in the project controller
-        public void ReadInputFieldClassicalModelType()
+        public bool ReadInputFieldClassicalModelType()
         {
-            string userInput = inputField.text; // Get the text from the Input Field
+            string userInput = inputField2.text; // Get the text from the Input Field
             // set the value of classical model type in the project controller
             try
             {
                 if (int.Parse(userInput) >= 0 && int.Parse(userInput) <= 2)
                 {
                     projectController.classicalModelType = int.Parse(userInput);
-                    emptyErrorText();
-
+                    errorText2.text = "";
+                    return true;
                 }
                 else
                 {
-                    errorText.text = "Invalid Model Type. Please enter a number between 0 and 2";
+                    errorText2.text = "Invalid Model Type. Please enter a number between 0 and 2";
                 }
             }
             catch (System.Exception e)
             {
-                errorText.text = "Invalid Model Type. Please enter a number between 0 and 2";
+                errorText2.text = "Invalid Model Type. Please enter a number between 0 and 2";
             }
             Debug.Log("User Input: " + userInput);
+            return false;
         }
         // read the input field value and set the value of feature extraction type in the project controller
-        public void ReadInputFieldfeatureExtractionTypeImg()
+        public bool ReadInputFieldfeatureExtractionTypeImg()
         {
-            string userInput = inputField.text; // Get the text from the Input Field
+            string userInput = inputField1.text; // Get the text from the Input Field
             // set the value of feature extraction type in the project controller
             // check if the input is 0, 1 or 2
             try
@@ -106,19 +210,28 @@ namespace GlobalAssets.UI
                 if (int.Parse(userInput) >= 0 && int.Parse(userInput) <= 2)
                 {
                     projectController.featureExtractionTypeImg = int.Parse(userInput);
-                    emptyErrorText();
-
+                    errorText1.text = "";
+                    return true;
                 }
                 else
                 {
-                    errorText.text = "Invalid Feature Type. Please enter a number between 0 and 2";
+                    errorText1.text = "Invalid Feature Type. Please enter a number between 0 and 2";
                 }
             }
             catch (System.Exception e)
             {
-                errorText.text = "Invalid Feature Type. Please enter a number between 0 and 2";
+                errorText1.text = "Invalid Feature Type. Please enter a number between 0 and 2";
+            }
+            return false;
+        }
+        public void ReadInputClassical()
+        {
+            bool featureBool = ReadInputFieldfeatureExtractionTypeImg();
+            bool modelBool = ReadInputFieldClassicalModelType();
+            if (featureBool && modelBool)
+            {
+                projectController.modelCategory = 0; //classical
             }
         }
-
     }
 }
--- Assets/Survival/Scripts/MenuStuff.cs
+++ Assets/Survival/Scripts/MenuStuff.cs
@@ -14,18 +14,96 @@ namespace Survival
         }
         public void B_LoadScene()
         {
+            if (nextSceneName == "SampleScene")
+            {
+                try
+                {
+                    GameObject.FindGameObjectWithTag("music").GetComponent<MusicControl>().StopMusic();
+                }
+                catch (System.Exception e)
+                {
+                    Debug.Log(e);
+                }
+            }
             SceneManager.LoadScene(nextSceneName);
+            Destroy(PlayerController.Instance.gameObject);
+
+            // Get the current active scene
+            // Scene currentScene = SceneManager.GetActiveScene();
+            // // Destroy the current scene
+            // SceneManager.UnloadSceneAsync(currentScene);
+            // StartCoroutine(LoadSceneAndUnloadCurrent(nextSceneName));
+
         }
 
         public void LoadScene(string sceneName)
         {
+            if (sceneName == "SampleScene")
+            {
+                try
+                {
+                    GameObject.FindGameObjectWithTag("music").GetComponent<MusicControl>().StopMusic();
+                }
+                catch (System.Exception e)
+                {
+                    Debug.Log(e);
+                }
+            }
+            // Get the current active scene
+            // Scene currentScene = SceneManager.GetActiveScene();
+            // // Destroy the current scene
+            // SceneManager.UnloadSceneAsync(currentScene);
             SceneManager.LoadScene(sceneName);
+            Destroy(PlayerController.Instance.gameObject);
 
+            // StartCoroutine(LoadSceneAndUnloadCurrent(sceneName));
+
+        }
+        private IEnumerator LoadSceneAndUnloadCurrent(string sceneName)
+        {
+            // Get the current active scene
+            Scene currentScene = SceneManager.GetActiveScene();
+
+            // Load the new scene asynchronously
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+            // Wait until the new scene is fully loaded
+            while (!asyncLoad.isDone)
+            {
+                yield return null;
+            }
+
+            // Set the newly loaded scene as the active scene
+            Scene newScene = SceneManager.GetSceneByName(sceneName);
+            SceneManager.SetActiveScene(newScene);
+
+            // Unload the previous scene asynchronously
+            AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync(currentScene);
+
+            // Wait until the previous scene is fully unloaded
+            while (!asyncUnload.isDone)
+            {
+                yield return null;
+            }
         }
         public void B_QuitGame()
         {
             // Application.Quit();
+            try
+            {
+                GameObject.FindGameObjectWithTag("music").GetComponent<MusicControl>().StopMusic();
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log(e);
+            }
             SceneManager.LoadScene("Lobby");
+            Destroy(PlayerController.Instance.gameObject);
+            // // Get the current active scene
+            // Scene currentScene = SceneManager.GetActiveScene();
+            // // Destroy the current scene
+            // SceneManager.UnloadSceneAsync(currentScene);
+            // StartCoroutine(LoadSceneAndUnloadCurrent("Lobby"));
 
         }
     }
